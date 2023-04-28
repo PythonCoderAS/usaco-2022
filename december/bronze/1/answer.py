e1b8b178f11bab5b90b2427cca7290202d3eb36d
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-04-27 20:50:50 -0400

    Original solution

---
+++ december/bronze/1/answer.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+n = int(input()) # First line is how many numbers there are
+numbers = input().strip().split() # Second line is the numbers
+assert len(numbers) == n # Make sure the length of the list is n
+numbers = [int(i) for i in numbers] # Convert the numbers to integers
+counts = defaultdict(int)
+prices = defaultdict(list)
+for num in numbers:
+    counts[num] += 1
+current_excluded = 0
+for price, num_excluded in counts.items():
+    money_made = (n - current_excluded) * price
+    prices[money_made].append(price)
+    current_excluded += num_excluded
+
+max_made = sorted(prices.keys())[-1]
+prices[max_made].sort()
+print(f"{max_made} {prices[max_made][0]}")
